1750e1eb292fa4e3a176bb500ad5624d20a58f95
Gregory Maldonado <gmaldona@oswego.edu>
2022-03-17 02:32:14 -0400

    Added end packet

---
+++ src/main/scala/packets/END.scala
@@ -0,0 +1,30 @@
+package edu.oswego.cs.gmaldona
+package packets
+import opcodes.Opcode.Opcode
+
+import opcodes.Opcode
+
+/*
+                  END PACKET
+
+            |----------------|-------------|
+            |                |             |
+            |     opcode     |   Block #   |
+            |                |             |
+            |----------------|-------------|
+
+                  2 Bytes          2 Bytes
+
+      An end packet signals the end of transmission
+ */
+
+
+case class END(blockNumber: Int) extends Packet {
+    override var opcode: Opcode = Opcode.END
+
+    override def getBytes: Array[Byte] = {
+        if (blockNumber <= Byte.MaxValue) Array(0.toByte, opcode.id.toByte, 0.toByte, blockNumber.toByte)
+        else Array(0.toByte, opcode.id.toByte).:++(BigInt(blockNumber).toByteArray)
+    }
+
+}
